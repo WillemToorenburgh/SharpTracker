7b101b551cbdfe0e5f80ff489419aa2035125186
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2023-06-10 13:33:07 -0700

    Revert "Making Track a view model, part 3"
    This reverts commit 78cce8050b8206198346286c29bd999d3a9a8394.

--- TrackerCore/Pattern.cs
+++ TrackerCore/Pattern.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Collections.Generic;
-using SharpTracker.ViewModels;
 
 namespace SharpTracker.TrackerCore;
 
 public class Pattern
 {
     public string Name;
-    public List<TrackViewModel> Tracks;
+    public List<Track> Tracks;
 
     private int _length = 32;
     public int Length
