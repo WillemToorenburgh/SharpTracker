899c46b5e8c4a246f178a70837fec08688d449d1
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2023-06-10 13:17:25 -0700

    Making Track a view model, part 4

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace SharpTracker.ViewModels;
+﻿using SharpTracker.TrackerCore;
+using System.Collections.ObjectModel;
+
+namespace SharpTracker.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+    public ObservableCollection<TrackViewModel> DummyTracks;
+
+    public MainWindowViewModel()
+    {
+        DummyTracks = new ObservableCollection<TrackViewModel>
+        {
+            new TrackViewModel(new {}, default),
+            new TrackViewModel(new {}, default)
+        };
+    }
 }
