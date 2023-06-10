b41d686fc4834a7d0056e90853febd36405ed9c8
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2023-06-10 13:33:07 -0700

    Revert "Making Track a view model, part 4"
    This reverts commit 899c46b5e8c4a246f178a70837fec08688d449d1.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,6 @@
-﻿using SharpTracker.TrackerCore;
-using System.Collections.ObjectModel;
-
-namespace SharpTracker.ViewModels;
+﻿namespace SharpTracker.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
-
-    public ObservableCollection<TrackViewModel> DummyTracks;
-
-    public MainWindowViewModel()
-    {
-        DummyTracks = new ObservableCollection<TrackViewModel>
-        {
-            new TrackViewModel(new {}, default),
-            new TrackViewModel(new {}, default)
-        };
-    }
 }
